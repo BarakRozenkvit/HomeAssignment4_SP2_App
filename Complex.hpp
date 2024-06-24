69fd049e88178c350809beb5f87fd159ce82d06a
BarakRozenkvit <155371601+BarakRozenkvit@users.noreply.github.com>
2024-06-24 17:51:57 +0300

    upload complex.hpp

--- Complex.hpp
+++ Complex.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -7,11 +8,24 @@ class Complex{
     int _imag;
 
 public:
+    Complex(){};
     Complex(int real,int imag):_real(real),_imag(imag){};
 
+    bool operator==(Complex& comp){
+        return _imag == comp._imag && _real == comp._real;
+    }
 
+    // TODO: what is < between complex numbers
+    bool operator<(Complex& other){
+        return _real < other._real && _imag < other._imag;
+    }
 
     friend string to_string(Complex& complex){
-        return to_string(complex._real) + "+" + to_string(complex._imag) +"i";
+           return to_string(complex._real) + "+" + to_string(complex._imag) +"i";
+       }
+
+    friend ostream& operator<<(ostream& out,Complex& comp){
+        out << to_string(comp);
+        return out;
     }
 };
